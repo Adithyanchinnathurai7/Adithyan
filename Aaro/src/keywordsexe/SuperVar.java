1145ce42704e6f17fa6005874173d20348efa4c8
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-06-09 14:38:22 +0530

    9/6

--- Aaro/src/keywordsexe/SuperVar.java
+++ Aaro/src/keywordsexe/SuperVar.java
@@ -1,6 +1,6 @@
-package fullinheritance;
+package keywordsexe;
 
-public class inhrtnIbacco {
+public class SuperVar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
