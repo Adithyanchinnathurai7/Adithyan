2f761dd1d59cefc3119e72b2d5a657d8034a2f87
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-06-24 09:50:38 +0530

    20/

--- Aaro/src/iotestbook/Io3rd.java
+++ Aaro/src/iotestbook/Io3rd.java
@@ -1,18 +1,30 @@
 package iotestbook;
 import java.io.*;
-class onlyext implements FilenameFilter{
-String ext;
-public onlyext(String e)
-{   ext="."+e;   }
-public boolean accept(File dir, String name){
-return name.endsWith(ext);
- }    }
-class Dirlistonly{
-public static void main(String args[]){
-String dirname="c:/users";
-File f1=new File(dirname);
-FilenameFilter only=new onlyext("html");
-String s[]=f1.list(only);
-for (int i=0;i<s.length;i++){
-System.out.println(s[i]);   }    
-}    }
+
+	class onlyext implements FilenameFilter
+	{
+		String ext;
+		public onlyext(String e)
+		{   	
+			ext="."+e;  
+		}
+
+		public boolean accept(File dir, String name)
+		{
+
+			return name.endsWith(ext);
+		}   
+	}
+	class Dirlistonly{
+		public static void main(String args[])
+		{
+			String dirname="c:/users";
+			File f1=new File(dirname);
+			FilenameFilter only=new onlyext("html");
+			String s[]=f1.list(only);
+			for (int i=0;i<s.length;i++)
+			{
+				System.out.println(s[i]);   
+			}    
+		}    
+	}
