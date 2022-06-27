a90068f8f7e75acc5ba55dfca86f22dfb819248d
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-06-27 19:59:13 +0530

    nit

---
+++ Practies/src/main/java/aadipractice/ArgumentDemo.java
@@ -0,0 +1,18 @@
+package aadipractice;
+
+public class ArgumentDemo 
+{
+
+	public static void main(String[] args) 
+	{ 
+		// go to run configur
+		String n1=args[0]; //declare string value given syntax
+		System.out.println(n1);
+		System.out.println(args[0]);
+		System.out.println(".....................");
+		int n=Integer.parseInt(args[0]);
+		System.out.println(n);
+		System.out.println(args[0]);
+	}
+
+}
---
+++ Practies/src/main/java/chap6to10/class1.java
@@ -0,0 +1,12 @@
+package chap6to10;
+
+public class class1 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
